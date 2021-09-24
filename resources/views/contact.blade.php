4a7a99822d8dc439ba31a7cf3c512ea742254fd4
AlexisResses <alexis.resses@epitech.eu>
2021-09-24 22:31:48 +0200

    add style | contact form | maps

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
     <div class="container" id="containerContent">
-        je suis la page contact
+        <form action="" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Nom</label>
+                <input type="text" class="form-control" id="name" placeholder="Votre nom" value="{{ old('name')}}">
+              </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Adresse Mail</label>
+                <input type="email" class="form-control" id="email" placeholder="Votre adresse mail" value="{{ old('email')}}">
+              </div>
+              <div class="mb-3">
+                <label for="demande" class="form-label">Votre demande</label>
+                <textarea class="form-control" id="demande" rows="6" value="{{ old('demande')}}"></textarea>
+              </div>
+              <div class="col-12">
+                <button class="btn btn-primary" type="submit">Envoyer</button>
+              </div>
+        </form>
     </div>
 @endsection
--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
     <div class="container" id="containerContent">
-        je suis la location
+        <p>
+            Horaire d'ouverture : 
+
+            Lundi au Jeudi : 08h00-12h30 / 14h30–18h45
+            Vendredi : 08h00-18h45
+            Samedi : 08h00-18h00
+            
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11399.043112635276!2d1.4393997!3d44.4175544!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5f8370a9931ed5fa!2sLes%20Halles%20Du%20Pech%20Biel!5e0!3m2!1sfr!2sfr!4v1632514382645!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 @endsection
