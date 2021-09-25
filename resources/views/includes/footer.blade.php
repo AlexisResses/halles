8be062c16f92a444cf890d8e8b5621dfc1b165ff
AlexisResses <alexis.resses@epitech.eu>
2021-09-25 18:36:02 +0200

    update footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
 
 <div class="container-fuild text-center">
     <div class="card-footer text-muted">
-        Réalisé par <a href="https://arwebdeveloppement.fr/" target="_blank">ARWEBDEVELOPPEMENT</a> | Copyright 2021
+        Réalisé par <a href="https://arwebdeveloppement.fr/" target="_blank">AR WEB DEVELOPPEMENT</a> | Copyright 2021
       </div>
 </div>
